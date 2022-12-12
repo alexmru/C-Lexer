6bde7528e7f80e0e6f721ac6675e197823571a9c
Alex <lxndmrr@gmail.com>
2022-12-12 03:12:57 +0200

    Include ctype.h and string.h

--- lexer.c
+++ lexer.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 enum
 {
@@ -35,38 +37,58 @@ enum
     LESS
 }; // toate codurile atomilor din Quick, fara SPACE si COMMENT
 
+
 typedef struct{
 	int cod;		// codul atomului ( ID, INT, ...)
+	int linie;		// linia atomului respectiv
+	union{
+		char s[100];		// pentru ID-uri si stringuri
+		double r;				// pentru real
+		int i;						// pentru int
+		};
 	}Atom;
+
 Atom atomi[10000];	// vectorul cu atomii extrasi din fisierul de intrare
 int nAtomi;	// numarul de atomi din vectorul atomi
 
+
 char bufin[30001];
 char *pch;	// cursor la caracterul curent din bufin
+int linie=1;		// linia curenta; adaugata automat la atom de addAtom
 
 // la fiecare apel returneaza codul unui atom
 int getNextTk()			// get next token (atom lexical)
 {
 	int state=0;		// starea curenta
+	char buf[100];
+	int n=0;		// nr caractere din buf
 	for(;;){
 		char ch=*pch;	// caracterul curent
-		printf("#%d %c(%d)\n",state,ch,ch);	// pt debugging
+		printf("#%:d %c(%d)\n",state,ch,ch);	// pt debugging
 		// cate un case pentru fiecare stare din diagrama
 		switch(state){
 			case 0:
 				// testam toate tranzitiile posibile din acea stare
-				if(isalpha(ch)||ch=='_'){state=1;pch++;}
+				if(isalpha(ch)||ch=='_'){state=1;pch++;buf[n++]=ch;}
 				else if(isdigit(ch)){state=3;pch++;}
-				else if(ch==' '||ch=='\r'||ch=='\n'||ch=='\t'){pch++;}
+				else if(ch==' '||ch=='\r'||ch=='\n'||ch=='\t'){
+						pch++;
+						if(ch=='\n')linie++;
+						}
 				break;
 			case 1:
-				if(isalnum(ch)||ch=='_'){pch++;}
+				if(isalnum(ch)||ch=='_'){pch++;buf[n++]=ch;}
 				else{state=2;}
 				break;
 			case 2:
-				addAtom(ID);	// adauga atomul gasit in lista de atomi
+				buf[n]='\0';	// incheiere sir
+				if(strcmp(buf,"var")==0) addAtom(VAR);
+				else if(strcmp(buf,"if`")==0) addAtom(IF);
+				else{
+					addAtom(ID);	// id simplu
+					strcpy(atomi[nAtomi-1].s,buf);
+					}
 				return ID;
-			//..........................
 			default: printf("stare invalida %d\n",state);
 			}
 		}
